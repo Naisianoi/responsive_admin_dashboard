0e8024cc421bde6c305bffa3e3f2495e07bbedba
Gladys Naisianoi <64873921+Naisianoi@users.noreply.github.com>
2023-10-30 09:42:33 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lavender KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '36996',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
